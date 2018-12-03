bec609a705e0ec7c49b946baeb86eef06fbd012e
Heng <Heng@Hengs-Mac.local>
2018-12-02 23:58:58 -0500

    .

--- ext2_restore.c
+++ ext2_restore.c
@@ -94,7 +94,23 @@ void go_through_file(int parent_inode,char *victim_file_child_name){
                             if(inodes[possible->inode - 1].i_block[i] != 0){
                                 restore_block_bitmap(inodes[possible->inode - 1].i_block[i] - 1);    
                             }
-                            
+                        }
+                        if(inodes[possible->inode - 1].i_block[12] != 0){
+                            check_block_bitmap(inodes[possible->inode - 1].i_block[12] - 1);
+                            int *indirect_block = (int *)(disk +  EXT2_BLOCK_SIZE * (inodes[possible->inode - 1].i_block)[12]);
+                            for(int i = 0; i < 256; i++) {
+                                if(indirect_block[i] != 0) {
+                                    check_block_bitmap(indirect_block[i]);
+                                }
+                            }
+                            for(int i = 0; i < 256; i++) {
+                                if(indirect_block[i] == 0) {
+                                    restore_block_bitmap(inodes[possible->inode - 1].i_block[12] - 1);
+                                    return;
+                                }
+                                restore_block_bitmap(indirect_block[i] - 1);   
+                                
+                            }
                         }
                         restore_inode_bitmap(possible->inode - 1);
                         inodes[possible->inode - 1].i_links_count += 1;
